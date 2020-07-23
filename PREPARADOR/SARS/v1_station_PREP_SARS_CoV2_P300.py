ccc185bc71d119cf75dfec9d9c518b0da3271b0c
Ricardo Magalhaes <ricmag@MBPRM.local>
2020-07-23 16:48:21 +0100

    Updated

--- PREPARADOR/SARS/v1_station_PREP_SARS_CoV2_P300.py
+++ PREPARADOR/SARS/v1_station_PREP_SARS_CoV2_P300.py
@@ -5,7 +5,7 @@ import math
 
 # metadata
 metadata = {
-    'protocolName': 'V1 Station A LAL P300',
+    'protocolName': 'V1 Station Prep SARS CoV2 MagMax',
     'author': 'Ricmag <ricmags@sapo.pt>',
     'source': 'Custom Protocol Request',
     'apiLevel': '2.0'
@@ -19,75 +19,41 @@ TIP_TRACK = False
 def run(ctx: protocol_api.ProtocolContext):
 
     # load labware
+    ic_pk = ctx.load_labware(
+        'opentrons_96_aluminumblock_generic_pcr_strip_200ul', '9',
+        'chilled tubeblock for internal control and proteinase K (strip 1)').wells()[0]
 
-    source_racks = [
-        ctx.load_labware(
-            'opentrons_15_tuberack_falcon_15ml_conical', slot,
-            'source tuberack ' + str(i+1))
-        for i, slot in enumerate(['11', '10', '7', '4', '5', '6', '3'])
-    ]
+    binding_buffer = ctx.load_labware(
+        'nest_12_reservoir_15ml', '8', 'reagent reservoir 1')
+        
     dest_plate = ctx.load_labware(
-        'nest_96_wellplate_200ul_flat', '9', '96-wellplate sample plate')
+        'nest_96_wellplate_2ml_deep', '11', '96-deepwell sample plate')
+
+
+    # load tips
   
-    tips300 = [ctx.load_labware('opentrons_96_filtertiprack_200ul', '8')]
-  
+    tips300 = [ctx.load_labware('opentrons_96_filtertiprack_200ul', '5',
+                                    '200µl filter tiprack')]
+    tips20 = [ctx.load_labware('opentrons_96_filtertiprack_20ul', '6',
+                                    '20µl filter tiprack')]
 
     # load pipette
 
-    p300 = ctx.load_instrument('p300_single_gen2', 'left', tip_racks=tips300)
-    
-    
-
+    m300 = ctx.load_instrument('p300_multi_gen2', 'left', tip_racks=tips300)
+    m20 = ctx.load_instrument('p20_multi_gen2', 'right', tip_racks=tips20)
+  
     # setup samples
-    sources = [
-        well for rack in source_racks for well in rack.wells()][:NUM_SAMPLES]
-    dests_single = dest_plate.wells()[:NUM_SAMPLES]
+    dests_multi = dest_plate.rows()[0][:num_cols]
 
-    tip_log = {'count': {}}
-    folder_path = '/data/A'
-    tip_file_path = folder_path + '/tip_log.json'
-    if TIP_TRACK and not ctx.is_simulating():
-        if os.path.isfile(tip_file_path):
-            with open(tip_file_path) as json_file:
-                data = json.load(json_file)
-                if 'tips300' in data:
-                    tip_log['count'][p300] = data['tips300']
-                else:
-                    tip_log['count'][p300] = 0
+    # transfer internal control + proteinase K
+    for d in dests_multi:
+        pick_up(m20)
+        m20.transfer(15, ic_pk.bottom(2), d.bottom(10), air_gap=5,
+                     new_tip='never')
+        m20.air_gap(5)
+        m20.drop_tip()    
 
-    else:
-        tip_log['count'] = {p300: 0}#, m20: 0}
-
-    tip_log['tips'] = {
-        p300: [tip for rack in tips300 for tip in rack.wells()]
-    }
-    tip_log['max'] = {
-        pip: len(tip_log['tips'][pip])
-        for pip in [p300]
-    }
-
-    def pick_up(pip):
-        nonlocal tip_log
-        if tip_log['count'][pip] == tip_log['max'][pip]:
-            ctx.pause('Replace ' + str(pip.max_volume) + 'µl tipracks before \
-resuming.')
-            pip.reset_tipracks()
-            tip_log['count'][pip] = 0
-        pip.pick_up_tip(tip_log['tips'][pip][tip_log['count'][pip]])
-        tip_log['count'][pip] += 1
-
-    # transfer two controls
-    # **Incluir o labware (suporte de eppendorf)
-    # definir local branco
-    # if TRUE reta de calibração
-
-    # transfer sample
-    for s, d in zip(sources, dests_single):
-        pick_up(p300)
-        p300.transfer(SAMPLE_VOLUME, s.bottom(5), d.bottom(5), air_gap=20,
-                      mix_before=(2, 200), new_tip='never')
-        p300.air_gap(20)
-        p300.drop_tip()
+  
 
     
 
